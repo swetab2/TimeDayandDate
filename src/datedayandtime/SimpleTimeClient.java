f81b7708d624a353dbb5e9b25296c6ab9a8c7c8a
HypemaxPC-11 <swetabh26@gmail.com>
2018-12-26 15:03:11 +0530

    Time and date project commit

--- src/datedayandtime/SimpleTimeClient.java
+++ src/datedayandtime/SimpleTimeClient.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package datedayandtime;
 
 import java.time.*;
 import java.lang.*;
 import java.util.*;
-
-
-
 public class SimpleTimeClient implements TimeClient{
-    
      private LocalDateTime dateAndTime;
     
     public SimpleTimeClient() {
